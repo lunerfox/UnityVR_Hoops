de875f27531e8b28c547c5bf6aeb6374b4764bb9
lunerfox <lunerfox@gmail.com>
2016-06-02 20:59:43 -0700

    Initial commit. Created a field using the cube object and a "basketball" object that's pre-fabbed. Created ballSpawner.cs script that instances a ball on space bar.

---
+++ VR_Hoops/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+        print("Ball Spawner Active");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Creates a new ball.
+            GameObject instance = Instantiate(ballPrefab);
+        }
+	}
+}
