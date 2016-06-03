dbe9ff27580d1d4df6cdc9c53f88901895344c25
lunerfox <lunerfox@gmail.com>
2016-06-02 21:28:17 -0700

    Added logic to provide some velocity to the ball based on the direction given in the arrow keys. Added Cam Rotation script and had the camera rotate 1 degree every frame.

--- VR_Hoops/Assets/Scripts/BallSpawner.cs
+++ VR_Hoops/Assets/Scripts/BallSpawner.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BallSpawner : MonoBehaviour {
 
     public GameObject ballPrefab;
+    const float velocity_scale = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,33 @@ public class BallSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             //Creates a new ball.
             GameObject instance = Instantiate(ballPrefab);
+            Rigidbody instance_RB = instance.GetComponent<Rigidbody>();
+            instance_RB.velocity = velocity_scale*Vector3.left;
         }
-	}
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Creates a new ball.
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody instance_RB = instance.GetComponent<Rigidbody>();
+            instance_RB.velocity = velocity_scale * Vector3.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //Creates a new ball.
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody instance_RB = instance.GetComponent<Rigidbody>();
+            instance_RB.velocity = velocity_scale * Vector3.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //Creates a new ball.
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody instance_RB = instance.GetComponent<Rigidbody>();
+            instance_RB.velocity = velocity_scale * Vector3.back;
+        }
+    }
 }
