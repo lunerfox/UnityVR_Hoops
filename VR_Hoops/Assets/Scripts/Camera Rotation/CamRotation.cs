4f01bc81cc050ceefd2d5f9421b233b2154ae345
lunerfox <lunerfox@gmail.com>
2016-06-03 10:49:09 -0700

    Updated camRotation script to include both X and Y rotation. Created a Body Parent object to the Camera to gimbal the camera movement.

---
+++ VR_Hoops/Assets/Scripts/Camera Rotation/CamRotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRotation : MonoBehaviour
+{
+    int invertMouseY = -1;
+    float mouseSensitivity = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = transform.localRotation * Quaternion.Euler(0, Input.GetAxis("Mouse X")* mouseSensitivity, 0); ;
+        Camera head = GetComponentInChildren<Camera>();
+        head.transform.localRotation = head.transform.localRotation * Quaternion.Euler(invertMouseY * Input.GetAxis("Mouse Y")* mouseSensitivity, 0, 0);
+    }
+
+}
