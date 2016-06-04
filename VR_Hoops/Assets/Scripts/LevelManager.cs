c60a676ae607d8275f310e97cae21994b8863f6c
lunerfox <lunerfox@gmail.com>
2016-06-04 10:15:58 -0700

    Added scene progression logic. Game now connects to a menu and a game over screen. Level Manager now has a "Time Till Next Level" public variable. 0 is to never automatically progress, while values greater then 0 will progress.

---
+++ VR_Hoops/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+enum GameStates {Menu, Game, GameOver};
+
+public class LevelManager : MonoBehaviour {
+
+    int currentIndex;
+    public float timeTillNextLevelcfg = 0.0f;
+    float timeTillNextLevel = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        // Load Scene
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+        // Reset the clock
+        timeTillNextLevel = timeTillNextLevelcfg;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Return)) {
+            // Load Current Scene + 1 (Next Scene)
+            SceneManager.LoadScene(currentIndex + 1);
+        }
+
+        //While the game is being played, count down the clock.
+        if (timeTillNextLevelcfg > 0) {
+            timeTillNextLevel -= Time.deltaTime;
+            if (timeTillNextLevel < 0) {
+                loadNextScene();
+            }
+        }
+	}
+
+    public void loadNextScene() {
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void loadPreviousScene() {
+        SceneManager.LoadScene(currentIndex - 1);
+    }
+
+    public void loadStartScene() {
+        SceneManager.LoadScene(0);
+    }
+}
