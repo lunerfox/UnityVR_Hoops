d32223cae276159eb40660b33d926b7251cd5e1c
lunerfox <lunerfox@gmail.com>
2016-06-04 13:47:40 -0700

    Changed UI to be compatible with VR systems. Canvas has been changed to World View (vs. Overlay)

---
+++ VR_Hoops/Assets/Scripts/Scoring/GameOverScoreDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverScoreDisplay : MonoBehaviour {
+
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "" + FindObjectOfType<ScoreAbsorber>().getScore();
+    }
+}
---
+++ VR_Hoops/Assets/Scripts/Scoring/ScoreAbsorber.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAbsorber : MonoBehaviour {
+
+    public uint score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        Scorekeeper oldKeeper = FindObjectOfType<Scorekeeper>();
+        if (oldKeeper) {
+            score = oldKeeper.getScore();
+            Destroy(oldKeeper.gameObject);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public uint getScore()
+    {
+        return score;
+    }
+}
